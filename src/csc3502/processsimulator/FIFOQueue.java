08cde1b2104d38b93627b4fe2dcc304eb8510fd1
J-Sexton <josh.b.sexton@gmail.com>
2017-02-09 23:02:10 -0500

    Final FIFOQueue, changed data structure, with changes made to header and

--- src/csc3502/processsimulator/FIFOQueue.java
+++ src/csc3502/processsimulator/FIFOQueue.java
@@ -20,97 +20,9 @@ public class FIFOQueue implements csc3502.processsimulator.Queue {
 	Node head = new Node(null);
 	Node pointer = head;
 	//public LinkedList <Node> fi = new LinkedList<Node>((Collection<? extends Node>) head);
-	public Queue<Node>q = new Queue<Node>() {
-        @Override
-        public boolean add(Node node) {
-            return false;
-        }
 
-        @Override
-        public boolean offer(Node node) {
-            return false;
-        }
 
-        @Override
-        public Node remove() {
-            return null;
-        }
 
-        @Override
-        public Node poll() {
-            return null;
-        }
-
-        @Override
-        public Node element() {
-            return null;
-        }
-
-        @Override
-        public Node peek() {
-            return null;
-        }
-
-        @Override
-        public int size() {
-            return 0;
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
-
-        @Override
-        public boolean contains(Object o) {
-            return false;
-        }
-
-        @Override
-        public Iterator<Node> iterator() {
-            return null;
-        }
-
-        @Override
-        public Object[] toArray() {
-            return new Object[0];
-        }
-
-        @Override
-        public <T> T[] toArray(T[] a) {
-            return null;
-        }
-
-        @Override
-        public boolean remove(Object o) {
-            return false;
-        }
-
-        @Override
-        public boolean containsAll(Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(Collection<? extends Node> c) {
-            return false;
-        }
-
-        @Override
-        public boolean removeAll(Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public boolean retainAll(Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public void clear() {
-
-        }
-    };
 
 	class Node {
 		Process process;
@@ -123,29 +35,51 @@ public class FIFOQueue implements csc3502.processsimulator.Queue {
 
 	}
 
-	//@Override
+	@Override
 	public void offer(Process p) {
-		/*Node previous= fi.getLast();
-		Node n = new Node(p);
-		fi.add(n);
-		previous.next=n;*/
-		Node n = new Node(p);
-		q.offer(n);
-		head = n;
+Node temp = new Node(p);
+		if(isEmpty()){
+		    head = temp;
 
+
+        }
+
+        else{
+
+			if(head.next == null){
+				head.next = temp;
+			}
+			pointer.next = temp;
+        }
+    pointer = temp;
+		pointer.next = null;
 
 	}
 
 	@Override
 	public Process poll() {
-		return head.process;
+
+        if (isEmpty()) {
+            return null;
+        }
+        else{
+            Process p = head.process;
+            head = head.next;
+            return p;
+        }
+
+
 	}
 
 	@Override
 	public boolean isEmpty() {
-	    if(head.process == null | head == null){
+	    if(head == null){
 	    	return true;
 		}
+
+		else if (head.process == null){
+	        return true;
+        }
 		else return false;
 	}
 
